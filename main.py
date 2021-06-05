9cddbfe190555d9e268001c0004b85052af7ee71
josefroylan.malaga.contractor <josefroylan.malaga.contractor@bbva.com>
2021-06-05 11:30:25 +0200

    Logger added

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+import logging
+
+# DEBUG, INFO, WARNING (default), 'ERROR', 'CRITICAL'
+
+logging.basicConfig(level=logging.DEBUG, filename='webhook.log',
+        format='%(levelname)s:%(asctime)s:%(message)s')
 
 app = Flask(__name__)
 
 @app.post('/webhooks')
 def webhooks():
     with open('webhook.log', 'a') as log:
-        params = request.get_json()
-        log.write(f'{datetime.now()}\n')
-        log.write(f'    {params}\n')
-    return jsonify(params)
+        logging.debug(request.get_json())
+        logging.debug(request.headers)
+    return jsonify({})
